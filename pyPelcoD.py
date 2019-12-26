5634ba54c8d6e4bcceb9e28280bde5ca6dd91cd7
BG6WRI <bg6wri@gmail.com>
2019-12-26 20:54:22 +0800

    #
    #

---
+++ pyPelcoD.py
@@ -0,0 +1,39 @@
+import serial
+#
+
+#
+up		=	0xff,0x01,0x00,0x08,0x00,0xff,0x08
+down	=	0xff,0x01,0x00,0x10,0x00,0xff,0x10
+left	=	0xff,0x01,0x00,0x04,0xff,0x00,0x04
+right	=	0xff,0x01,0x00,0x02,0xff,0x00,0x02
+stop	=	0xff,0x01,0x00,0x00,0x00,0x00,0x01
+
+'''
+how to use:
+from Pelco_D import Tracker
+Tracker = Tracker("/dev/ttyUSB0")
+'''
+
+
+class Tracker:
+
+	def __init__(self, add):
+		self.ser=serial.Serial("/dev/ttyUSB0",2400,timeout=0.5)
+
+	def up(self):
+		self.ser.write(up)
+
+	def down(self):
+		self.ser.write(down)
+
+	def left(self):
+		self.ser.write(left)
+
+	def right(self):
+		self.ser.write(right)
+
+	def stop(self):
+		self.ser.write(stop)
+
+	def write(self, data):
+		self.ser.write(data)
